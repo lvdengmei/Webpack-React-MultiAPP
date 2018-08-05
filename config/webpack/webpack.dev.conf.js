ac586b58a9c3da3ed41275c6610c801bc7ccff72
lvdengmei <lvdengmei@163.com>
2018-08-05 16:05:12 +0800

    webpack4 开发环境打包性能初提升

--- config/webpack/webpack.dev.conf.js
+++ config/webpack/webpack.dev.conf.js
@@ -47,6 +47,7 @@ let config = merge(baseWebpackConfig,{
             {
                 test:/\.(js|jsx)$/,
                 use:[
+                    'cache-loader',
                     'babel-loader'
                 ],
                 include:[
